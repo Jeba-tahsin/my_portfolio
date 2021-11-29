9eda1c7709fd1403a9d660bc29f824a92150cd24
Jeba-tahsin <jebatahsin500@.com>
2021-11-29 23:50:16 +0600

    live

--- src/pages/Home/Projects.js
+++ src/pages/Home/Projects.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import blood from '../../images/blood.PNG'; 
+import pottery from '../../images/pottery.PNG'; 
+import advanture from '../../images/advanture.PNG'; 
 
 const Projects = () => {
     return (
@@ -9,33 +11,42 @@ const Projects = () => {
                 <div class="col">
                     <div class="card">
                     <div>
-                    <img src='' class="card-img-top img-fluid image" alt="..."/>
+                    <img src={blood} class="card-img-top img-fluid image" alt="..."/>
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Web Development</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.This content is a little bit longer.</p>
+                        <h5 class="card-title">Red Cross</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div>
+                    <a href="https://red-cross-6853e.web.app/"><button className='btn btn-info mt-4'>View Website</button></a>
                     </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card">
                     <div>
-                    <img src='' class="card-img-top img-fluid image" alt="..."/>
+                    <img src={pottery} class="card-img-top img-fluid image" alt="..."/>
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Web Design</h5>
+                        <h5 class="card-title">Pottery</h5>
                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div>
+                    <a href="https://pottery-12214.web.app/"><button className='btn btn-info mt-4'>View Website</button></a>
                     </div>
                     </div>
                 </div>
                 <div class="col">
                     <div class="card">
                     <div>
-                    <img src='' class="card-img-top img-fluid image" alt="..."/>
+                    <img src={advanture} class="card-img-top img-fluid image" alt="..."/>
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Bug Fixing</h5>
+                        <h5 class="card-title">Advanture</h5>
                         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div>
+                    <a href="https://assignment-11-da68c.web.app/"><button className='btn btn-info mt-4'>View Website</button></a>
                     </div>
                     </div>
                 </div>
